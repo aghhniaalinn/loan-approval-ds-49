77bb111d0c32c88ffe115d45156d06ccf4172997
aghhniaalinn <alinnisa21@gmail.com>
2025-07-16 20:54:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def predict(gender, married, dependent, education, self_employed, applicant_inco
     mar = 0 if married == "Yes" else 1  
     edu = 0 if education == "Graduate" else 1  
     sem = 0 if self_employed == "Yes" else 1
-    pro = 0 if property_area == "Semiurban" else 1 if property_are == "Urban" else 2
+    pro = 0 if property_area == "Semiurban" else 1 if property_area == "Urban" else 2
                              
     #Making prediction
     prediction = Logistic_Regression_Model(
